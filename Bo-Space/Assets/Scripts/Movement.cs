37501881aeb83818bd0a19d5caa79d931650eff9
Ivo <34885@ma-web.nl>
2023-05-11 09:38:04 +0200

    added sprint

--- Bo-Space/Assets/Scripts/Movement.cs
+++ Bo-Space/Assets/Scripts/Movement.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    [SerializeField] private float speed = 1519f;
-    [SerializeField] private float rotSpeed = 200f;
+    [SerializeField] internal float speed = 1600f;
+    [SerializeField] internal float rotSpeed = 100f;
+    public float sprint = 25f;
     private Rigidbody rb;
+    public bool isMoving = false;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody>();
-        //onFloor = false;
     }
 
     // Update is called once per frame
@@ -24,5 +26,18 @@ public class Movement : MonoBehaviour
         rb.velocity = newVel;
         float rot = Input.GetAxis("Horizontal") * rotSpeed * Time.deltaTime;
         rb.transform.Rotate(new Vector3(0, rot, 0));
+
+        if (Input.GetKeyDown("w"))
+        {
+            isMoving = true;
+        }
+        if (Input.GetKeyUp("w"))
+        {
+            isMoving = false;
+        }
+        if (Input.GetKey(KeyCode.LeftShift) & isMoving == true) 
+        {
+            transform.position += transform.forward * Time.deltaTime * sprint;
+        }
     }
 }
